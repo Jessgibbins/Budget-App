72d5998f140b81efa3b33e8c7fe33070e845e619
Jessica Gibbins <jessagibbins@gmail.com>
2019-12-22 13:57:33 -0500

    Add js modules

---
+++ starter/app.js
@@ -0,0 +1,29 @@
+// BUDGET CONTROLLER
+
+const budgetController = (function() {
+
+    
+
+})();
+
+
+
+// UI CONTROLLER
+
+const UIController = (function() {
+
+
+
+})();
+
+
+
+// GLOBAL APP CONTROLLER
+
+const controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        
+    });
+
+})(budgetController, UIController);
